6ef162c8e1b92a0fb4c1d539bd041408dfd41efc
Leonardo de-Oliveira-Martins (QIB) <Leonardo.de-Oliveira-Martins@quadram.ac.uk>
2020-01-30 17:29:55 +0000

    working context-specific

--- src/hopo_counter.c
+++ src/hopo_counter.c
@@ -8,9 +8,10 @@
 KSEQ_INIT(gzFile, gzread);
 
 static uint8_t dna_in_2_bits[256][2] = {{0xff}};
+static char bit_2_dna[] = {'A', 'C', 'G', 'T'};
 static void initialize_dna_to_bit_tables (void);
 hopo_counter new_hopo_counter (void);
-void update_hopo_counter_from_seq (hopo_counter hc, char *seq, int seq_length, int kmer_size);
+void update_hopo_counter_from_seq (hopo_counter hc, char *seq, int seq_length, int kmer_size, int min_hopo_size);
 
 hopo_counter
 new_hopo_counter_from_file (const char *filename)
@@ -19,8 +20,8 @@ new_hopo_counter_from_file (const char *filename)
   gzFile fp = gzopen (filename, "r");
   kseq_t *seq = kseq_init (fp);
   hopo_counter hc = new_hopo_counter ();
-  while ((i = kseq_read (seq)) >= 0) update_hopo_counter_from_seq (hc, seq->seq.s, seq->seq.l, 3); // seq->name.s, seq->seq.l, seq->qual.l
-  kseq_destroy(seq);
+  while ((i = kseq_read (seq)) >= 0) update_hopo_counter_from_seq (hc, seq->seq.s, seq->seq.l, 5, 3); // seq_l, kmer_size, min_homopol_size 
+  kseq_destroy(seq); // other kseq_t parameters: seq->name.s, seq->seq.l, seq->qual.l
   gzclose(fp);
   return hc;
 }
@@ -59,23 +60,36 @@ initialize_dna_to_bit_tables (void)
 }
 
 void
-update_hopo_counter_from_seq (hopo_counter hc, char *seq, int seq_length, int kmer_size)
+update_hopo_counter_from_seq (hopo_counter hc, char *seq, int seq_length, int kmer_size, int min_hopo_size)
 {
-  int i, j, *freq, count_same = 0, start_mono = -1;
-  uint8_t prev = 5; // dna_in_2_bits[][] goes from 0 to 4
+  int i, j, k, *freq, count_same = 0, start_mono = -1;
+  uint8_t context[2 * kmer_size]; 
   char prev_char = '$';
 
-  printf ("DBG:: %s\n", seq);
+  printf ("\nDBG:: %s (%d)\n", seq, seq_length);
   count_same = 0;
   for (i = 0; i < (seq_length - kmer_size); i++) {
     if (seq[i] == prev_char) {
       count_same++;
-      if ((count_same > 2) && (start_mono > kmer_size)) {
+      if ((count_same > min_hopo_size) && (start_mono >= kmer_size)) {
         while (i < (seq_length-kmer_size-1) && (seq[i+1] == prev_char)) { i++; count_same++; }
-        for (j=start_mono; j <= i; j++) printf ("%c", seq[j]);
-        printf ("\n");
+        for (j=start_mono; j <= i; j++) printf ("%c", seq[j]); //DEBUG
+        if (dna_in_2_bits[prev_char][0] < dna_in_2_bits[prev_char][1]) { // A or C : forward strand
+          k = 0;
+          for (j = start_mono - kmer_size; j < start_mono; j++) context[k++] = dna_in_2_bits[ seq[j] ][0]; 
+          for (j = i + 1; j <= i + kmer_size; j++) context[k++] = dna_in_2_bits[ seq[j] ][0]; 
+          printf (" + ");
+        }
+        else if (dna_in_2_bits[prev_char][0] > dna_in_2_bits[prev_char][1]) { // T/U or G : reverse strand
+          k = 0; // it would be easier to copy above, but with k--; however I wanna implement rolling hash in future
+          for (j = i + kmer_size; j > i; j--) context[k++] = dna_in_2_bits[ seq[j] ][1]; 
+          for (j = start_mono - 1; j >= start_mono - kmer_size; j--) context[k++] = dna_in_2_bits[ seq[j] ][1]; 
+          printf (" - ");
+        } // elsif (dna[0] > dna[1]); notice that if dna[0] == dna[1] then do nothing (not an unambiguous base)
+        for (j=0; j < 2*kmer_size; j++) printf ("%c", bit_2_dna[ context[j] ]); //DEBUG
+        printf (" ( %3d %3d)\n", i, count_same);
       } // if (count_same>2) [i.e. we found valid homopolymer]
-    } else {
+    } else { 
       count_same = 1;
       prev_char = seq[i];
       start_mono = i;
