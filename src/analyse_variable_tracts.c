592c920ba63e2cb42cf955c4e10b4fd363150381
Leonardo de Oliveira Martins (QIB) <leomrtns@gmail.com>
2021-11-05 18:40:24 +0000

    sketch of VCF compressed files

--- src/analyse_variable_tracts.c
+++ src/analyse_variable_tracts.c
@@ -4,7 +4,7 @@
 
 #include "analyse_variable_tracts.h"
 
-file_compress_t initialise_vcf_file (char *name);
+file_compress_t initialise_vcf_file (char *outdir, char *name);
 
 void
 generate_vcf_files (genome_set_t g)
@@ -19,32 +19,42 @@ generate_vcf_files (genome_set_t g)
   biomcmc_warning ("ZLIB library not installed, will sabe VCF files uncompressed\n");
 #endif
 
-  for (i = 0; i < g->n_genome; i++) vcf[i] = initialise_vcf_file (g->genome[i]->name);
+  for (i = 0; i < g->n_genome; i++) vcf[i] = initialise_vcf_file (g->genome[0]->opt.outdir, g->genome[i]->name);
 
 
   for (i = 0; i < g->n_genome; i++) biomcmc_close_compress (vcf[i]);
 }
 
 file_compress_t
-initialise_vcf_file (char *name)
+initialise_vcf_file (char *outdir, char *name)
 {
   file_compress_t vcf = NULL;
   size_t l = strlen (name), buffer_size = 8192;
   char *s = biomcmc_malloc (buffer_size * sizeof (char));
-  strncpy (s, name, l);
-  strncpy (s + l, ".vcf", 4);
-  l += 4;
-  s[l] = '\0';
+
+  memset (s, '\0', sizeof (char) * buffer_size); // strcat starts at first null char 
+  strcpy (s, outdir);
+  strcat (s, name);
+  strcat (s, ".vcf");
 #ifdef HAVE_ZLIB
-  strncpy (s + l, ".gz", 3); // by adding suffix create_compress() can guess the library to use
-  s[l+3] = '\0';
+  strcat (s, ".gz"); // by adding suffix create_compress() can guess the library to use
 #endif
   vcf = biomcmc_create_compress_from_suffix (s); 
 
-  //memset (s, '\0', sizeof (char) * buffer_size); // also strcat(s, tmp) to concat tmp to first null of s
-  sprintf (s, "##fileformat=VCFv4.2\n#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t%s\n",name);
+  memset (s, '\0', sizeof (char) * buffer_size);
+  sprintf (s, "##fileformat=VCFv4.2\n##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">\n##INFO=<ID=TID,Number=A,Type=String,Description=\"tract ID\">\n");
+  biomcmc_write_compress (vcf, s);
+  sprintf (s, "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t%s\n",name);
   biomcmc_write_compress (vcf, s);
 
   if (s) free (s);
   return vcf;
 }
+
+/*
+void
+update_vcf_files_for_tid (genome_set_t g, int i1, int i2)
+{
+"NC_017280 44  . G A . PASS TID=tid001   GT  0"
+}
+*/
